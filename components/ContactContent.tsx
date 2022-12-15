b75cecb38143b1d840a92a65a466d650edf2f4c3
drfrank66613 <dm66613@gmail.com>
2022-12-15 13:10:14 +0700

    Create ContactContent with framer-motion animation

---
+++ components/ContactContent.tsx
@@ -0,0 +1,68 @@
+import { motion, Variants } from "framer-motion";
+
+const containerVariants: Variants = {
+  hidden: {
+    height: 0,
+    overflowY: "hidden",
+  },
+  visible: {
+    height: "100%",
+    transition: { duration: 0.5, when: "beforeChildren" },
+    transitionEnd: { overflowY: "auto" },
+  },
+};
+
+const contentVariants: Variants = {
+  hidden: {
+    opacity: 1,
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      delay: 0.5,
+      staggerChildren: 0.006,
+    },
+  },
+};
+
+const letterVariants: Variants = {
+  hidden: {
+    opacity: 0,
+    y: 50,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
+};
+
+const ContactContent = () => {
+  const text1 = `Contact`;
+
+  return (
+    <>
+      <motion.div
+        className="content-border border rounded-lg rounded-t-none p-4 w-full h-full overflow-y-auto"
+        variants={containerVariants}
+        initial="hidden"
+        animate="visible"
+      >
+        <motion.p
+          variants={contentVariants}
+          // initial="hidden"
+          // animate={animation}
+          className="tracking-widest text-justify"
+        >
+          {text1.split("").map((char, index) => (
+            <motion.span key={char + "-" + index} variants={letterVariants}>
+              {char}
+            </motion.span>
+          ))}
+        </motion.p>
+      </motion.div>
+      {/* <button onClick={() => cycleAnimation()}>Speed Up</button> */}
+    </>
+  );
+};
+
+export default ContactContent;
