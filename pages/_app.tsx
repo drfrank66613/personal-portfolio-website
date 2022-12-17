ffdd830e4950a0de64ef0588be02f0ce564ca033
drfrank66613 <dm66613@gmail.com>
2022-12-17 18:58:21 +0700

    Update _app.tsx
    -modify overall layout & styling
    -improve the animation of article element

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,39 +7,37 @@ import Head from "next/head";
 import Link from "next/link";
 import AboutContent from "../components/AboutContent";
 import { useRouter } from "next/router";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
+import { containerVariants } from "../utils/variants";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const links = ["about", "skills", "contact"];
-  const activeLinkStyle = "border rounded-lg rounded-b-none border-b-0 p-4";
   const currentPath = router.pathname;
   const initSelected = currentPath === "/" ? "about" : currentPath.slice(1);
+  const borderTopStyle =
+    currentPath === "/skills"
+      ? ""
+      : currentPath === "/contact"
+      ? "rounded-tr-none"
+      : "rounded-tl-none";
   const [selected, setSelected] = useState(initSelected);
 
-  // useEffect(() => {
-  //   const currentPath = router.pathname;
-  //   const initSelected =
-  //     currentPath === "/" ? "about" : router.pathname.slice(1);
-
-  //   setSelected(initSelected);
-  // }, []);
-
   return (
-    <>
-      <div className="flex h-screen px-10">
-        <aside className="absolute">
-          <MovingText />
-        </aside>
+    <div className="h-screen relative">
+      <aside className="my-7 mx-3 absolute">
+        <MovingText />
+      </aside>
 
-        <main className="mx-10 my-9 flex w-full">
-          <aside>
+      <main className="h-full px-11 py-7">
+        <div className="flex h-[50%] space-x-5">
+          <aside className="relative h-[110%] min-w-[19%] w-[19%] border rounded-lg">
             <Logo />
           </aside>
 
-          <section className="ml-9 w-full">
-            <nav className="flex justify-between w-full">
+          <section className="flex flex-col w-[79%]">
+            <nav className="flex justify-between">
               {links.map((link, index) => (
                 <div
                   key={index}
@@ -57,18 +55,33 @@ export default function App({ Component, pageProps }: AppProps) {
                         ease: "easeOut",
                       }}
                       layoutId="border"
-                      className="absolute inset-0 border border-b-0 rounded-t-lg z-0"
+                      className="absolute inset-0 -bottom-[1px] border border-b-[#1A1A1D] rounded-t-lg z-0"
                     />
                   ) : null}
                 </div>
               ))}
             </nav>
-            <article className="h-[46%]">
-              <Component {...pageProps} />
-            </article>
+            <AnimatePresence mode="wait">
+              <motion.article
+                className={`content-border h-full px-4 py-1 border ${borderTopStyle} rounded-lg overflow-auto`}
+                key={currentPath}
+                variants={containerVariants}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+              >
+                <Component {...pageProps} />
+              </motion.article>
+            </AnimatePresence>
           </section>
-        </main>
-      </div>
-    </>
+        </div>
+        <div>
+          <section>
+            <h1 className="text-right">Projects</h1>
+            <div>Contatiner</div>
+          </section>
+        </div>
+      </main>
+    </div>
   );
 }
