e190a6c6724b27fe47e394477f1286c005f5a938
drfrank66613 <dm66613@gmail.com>
2022-12-17 18:50:24 +0700

    Update MovingText.tsx
    remove unnecessary styles

--- components/MovingText.tsx
+++ components/MovingText.tsx
@@ -2,12 +2,12 @@ import Marquee from "react-fast-marquee";
 
 const MovingText = () => {
   return (
-    <h1
-      className="rotate-180 text-right my-10"
+    <h3
+      className="rotate-180 text-right"
       style={{ writingMode: "vertical-lr" }}
     >
       George's Portfolio
-    </h1>
+    </h3>
   );
 };
 
