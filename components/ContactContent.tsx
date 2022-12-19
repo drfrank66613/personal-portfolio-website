891a9bc576212aec5bfd93ca3eade1fec30001ba
drfrank66613 <dm66613@gmail.com>
2022-12-19 14:23:52 +0700

    Update ContactContent.tsx
    -add in form element with some inputs and textarea for Contact page
    -remove unnecessary code

--- components/ContactContent.tsx
+++ components/ContactContent.tsx
@@ -1,38 +1,23 @@
 import { motion, Variants, AnimatePresence } from "framer-motion";
 import {
   containerVariants,
+  contactContentVariants,
   contentVariants,
   letterVariants,
 } from "../utils/variants";
+import CustomInput from "./CustomInput";
 
 const ContactContent = () => {
-  const text1 = `Contact`;
-
   return (
-    <>
-      <div
-      // key="test"
-      // className="content-border border rounded-lg rounded-t-none p-4 w-full h-full overflow-y-auto"
-      // variants={containerVariants}
-      // initial="hidden"
-      // animate="visible"
-      // exit="exit"
-      >
-        <motion.p
-          variants={contentVariants}
-          // initial="hidden"
-          // animate={animation}
-          className="tracking-widest text-justify"
-        >
-          {text1.split("").map((char, index) => (
-            <motion.span key={char + "-" + index} variants={letterVariants}>
-              {char}
-            </motion.span>
-          ))}
-        </motion.p>
-      </div>
-      {/* <button onClick={() => cycleAnimation()}>Speed Up</button> */}
-    </>
+    <motion.form
+      variants={contactContentVariants}
+      className="h-full flex flex-col justify-evenly"
+    >
+      <input type="text" id="name" name="name" placeholder="Full Name" />
+      <input type="email" id="email" name="email" placeholder="Email" />
+      <input type="phone" id="phone" name="phone" placeholder="Phone" />
+      <textarea id="message" name="message" placeholder="Message" />
+    </motion.form>
   );
 };
 
