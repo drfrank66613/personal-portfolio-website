aece02f32b5514dbfaba32f65dc773acbe8718cc
drfrank66613 <dm66613@gmail.com>
2022-12-25 18:38:11 +0700

    Update _app.tsx
    implement logic of horizontal scroll of projects list while mouse moving

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,8 +8,18 @@ import Link from "next/link";
 import AboutContent from "../components/AboutContent";
 import { useRouter } from "next/router";
 import { AnimatePresence, motion } from "framer-motion";
-import { useEffect, useState } from "react";
+import {
+  MouseEvent,
+  TouchEvent,
+  BaseSyntheticEvent,
+  MouseEventHandler,
+  SyntheticEvent,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 import { containerVariants } from "../utils/variants";
+import { gsap } from "gsap";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -23,6 +33,55 @@ export default function App({ Component, pageProps }: AppProps) {
       ? "rounded-tr-none"
       : "rounded-tl-none";
   const [selected, setSelected] = useState(initSelected);
+
+  // Projects Section
+  // const [mouseXPos, setMouseXPos] = useState<number | null>(null);
+
+  const scroller = useRef<HTMLDivElement>(null);
+  const [contentWidth, setContentWidth] = useState<number>(0);
+  const [windowWidth, setWindowWidth] = useState<number>(0);
+  const [overflow, setOverflow] = useState<number>(0);
+  const [mapPosition, setMapPosition] = useState<Function>(Function);
+
+  const onResize = () => {
+    setWindowWidth(window.innerWidth);
+    setOverflow(contentWidth - windowWidth);
+    setMapPosition(() =>
+      gsap.utils.mapRange(0, windowWidth, overflow / 2, overflow / -2)
+    );
+  };
+
+  const onMouseMove = (e: MouseEvent) => {
+    if (overflow > 0) {
+      let x = e.clientX || 0;
+
+      gsap.to(scroller.current, {
+        duration: 1,
+        overwrite: true,
+        ease: "power3",
+        x: mapPosition(x),
+      });
+    }
+  };
+
+  useEffect(() => {
+    setContentWidth(scroller.current?.offsetWidth!);
+    setWindowWidth(window.innerWidth);
+    setOverflow(contentWidth - windowWidth);
+    setMapPosition(() =>
+      gsap.utils.mapRange(0, windowWidth, overflow / 2, overflow / -2)
+    );
+
+    console.log("windowWidth", windowWidth);
+    console.log("contentWidth", contentWidth);
+    console.log("overflow", overflow);
+
+    window.addEventListener("resize", onResize);
+
+    return () => {
+      window.removeEventListener("resize", onResize);
+    };
+  }, [windowWidth, contentWidth, overflow]);
 
   return (
     <div className="h-screen relative">
@@ -75,10 +134,31 @@ export default function App({ Component, pageProps }: AppProps) {
             </AnimatePresence>
           </section>
         </div>
-        <div>
-          <section>
-            <h1 className="text-right">Projects</h1>
-            <div>Contatiner</div>
+        <div
+          className="h-[50%] pt-2 bg-purple-500"
+          // onMouseMove={(e) => console.log(e)}
+        >
+          <section className="h-full flex flex-col">
+            <h1 className="text-right">{windowWidth}</h1>
+            <div
+              className="h-full w-full flex items-center justify-center overflow-hidden relative bg-gray-500"
+              // onLoadCapture={() => console.log("test")}
+              onMouseMove={onMouseMove}
+            >
+              <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+                <div
+                  className="flex justify-center items-center bg-white text-black text-center"
+                  ref={scroller}
+                >
+                  <div className="mx-96 border border-black">1</div>
+                  <div className="mx-96 border border-black">2</div>
+                  <div className="mx-96 border border-black">3</div>
+                  <div className="mx-96 border border-black">4</div>
+                  <div className="mx-96 border border-black">5</div>
+                  <div className="mx-96 border border-black">6</div>
+                </div>
+              </div>
+            </div>
           </section>
         </div>
       </main>
