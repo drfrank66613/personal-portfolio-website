bc39da291517d2a6463fdcbc19ba20bb99e29cd5
drfrank66613 <dm66613@gmail.com>
2022-12-30 18:16:43 +0700

    Update AboutContent.tsx
    -remove unnecessary text content
    -remove unnecessary code

--- components/AboutContent.tsx
+++ components/AboutContent.tsx
@@ -11,33 +11,20 @@ const AboutContent = () => {
   const [animation, cycleAnimation] = useCycle("visible", "animateFaster");
 
   const text1 = `I am George Kennedy, a software engineer who loves to create new things
-  and solve day-to-day problems with the help of technology. Currently interested in
-  working as a Full-stack Web Developer.`;
+  and solve day-to-day problems with the help of technology.`;
 
   const text2 = `Proficient in using HTML/CSS, JavaScript, React, Node/Express for
   building Full-Stack Web App. Having basic skills in AI (Python,
-  Tensorflow), IoT (Raspberry Pi - Python, Arduino - C++), Mobile App
-  Development (Android Studio - Java).`;
+    Tensorflow), IoT (Raspberry Pi - Python, Arduino - C++), Mobile App
+    Development (Android Studio - Java).`;
 
-  const text3 = `Lastly, it is a pleasure for me if
-  my applications can benefit society.`;
+  // const text3 = `  Currently interested in
+  // working as a Full-stack Web Developer.`;
 
   return (
     <>
-      <div
-      // className="bg-gray-500"
-      // key="test"
-      // className="content-border border rounded-lg rounded-t-none p-4 w-full h-full overflow-y-auto"
-      // variants={containerVariants}
-      // initial="hidden"
-      // animate="visible"
-      // exit="exit"
-      >
-        <motion.p
-          variants={textRevealVariants}
-          // initial="hidden"
-          // animate={animation}
-        >
+      <div>
+        <motion.p variants={textRevealVariants}>
           {text1.split("").map((char, index) => (
             <motion.span key={char + "-" + index} variants={letterVariants}>
               {char}
@@ -49,12 +36,12 @@ const AboutContent = () => {
               {char}
             </motion.span>
           ))}
-          <br />
+          {/* <br />
           {text3.split("").map((char, index) => (
             <motion.span key={char + "-" + index} variants={letterVariants}>
               {char}
             </motion.span>
-          ))}
+          ))} */}
         </motion.p>
       </div>
 
