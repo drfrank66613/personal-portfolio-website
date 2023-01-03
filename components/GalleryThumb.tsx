e6784a043dc225f2675bcb0f2c672c44a60b0aeb
drfrank66613 <dm66613@gmail.com>
2023-01-03 16:09:27 +0700

    Update GalleryThumb.tsx
    -modify GalleryThumbProps
    -modiy styles

--- components/GalleryThumb.tsx
+++ components/GalleryThumb.tsx
@@ -1,18 +1,14 @@
 import Image from "next/image";
 import { useState } from "react";
-
-type Gallery = {
-  src: string;
-  alt: string;
-};
+import type { ImageGallery } from "../data/projects";
 
 type GalleryThumbProps = {
   index: number;
-  gallery: Gallery;
-  viewImage: (image: Gallery) => void;
+  image: ImageGallery;
+  viewImage: (image: ImageGallery) => void;
 };
 
-const GalleryThumb = ({ index, gallery, viewImage }: GalleryThumbProps) => {
+const GalleryThumb = ({ index, image, viewImage }: GalleryThumbProps) => {
   const [isActive, setIsActive] = useState<boolean>(false);
 
   const toggleActive = () => {
@@ -23,7 +19,7 @@ const GalleryThumb = ({ index, gallery, viewImage }: GalleryThumbProps) => {
     <div
       onMouseEnter={toggleActive}
       onMouseLeave={toggleActive}
-      onClick={() => viewImage(gallery)}
+      onClick={() => viewImage(image)}
       className={
         index === 0
           ? "col-span-2 row-span-2 h-full w-full relative border rounded-lg cursor-pointer"
@@ -31,16 +27,14 @@ const GalleryThumb = ({ index, gallery, viewImage }: GalleryThumbProps) => {
       }
     >
       {isActive && (
-        <div className="absolute bg-black bg-opacity-50 flex justify-center items-center h-full w-full z-10">
-          <h2 className="bg-neutral-900 bg-opacity-80 text-white px-2 py-1 rounded-lg">
-            Click to view
-          </h2>
+        <div className="absolute bg-neutral-900 bg-opacity-90 rounded-lg flex justify-center items-center h-full w-full z-10">
+          <h2 className="text-white px-2 py-1 rounded-lg">Click to view</h2>
         </div>
       )}
 
       <Image
-        src={gallery.src}
-        alt={gallery.alt}
+        src={image.src}
+        alt={image.alt}
         fill
         style={{ objectFit: "cover", borderRadius: "0.5rem" }}
       />
