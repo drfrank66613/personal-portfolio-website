e3eaf076a0849e8fae417984745fa9df77077d4a
drfrank66613 <dm66613@gmail.com>
2023-01-03 16:08:24 +0700

    Create FullImageGallery.tsx

---
+++ components/FullImageGallery.tsx
@@ -0,0 +1,78 @@
+import Image from "next/image";
+import { useState } from "react";
+import { AiOutlineClose, AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+import type { ImageGallery } from "../data/projects";
+
+type FullImageGalleryProps = {
+  viewedImage: ImageGallery;
+  gallery: ImageGallery[];
+  closeImage: () => void;
+};
+
+const FullImageGallery = ({
+  viewedImage,
+  gallery,
+  closeImage,
+}: FullImageGalleryProps) => {
+  const [currentImage, setCurrentImage] = useState<ImageGallery>(viewedImage);
+
+  const nextImageIdx =
+    gallery.findIndex((image) => image.id === currentImage.id) + 1;
+  const nextImage = gallery.find((_, index) => nextImageIdx === index);
+
+  const prevImageIdx =
+    gallery.findIndex((image) => image.id === currentImage.id) - 1;
+  const prevImage = gallery.find((_, index) => prevImageIdx === index);
+
+  const changeImage = (image: ImageGallery) => {
+    setCurrentImage(image);
+  };
+
+  return (
+    <div className="inset-0  fixed h-screen w-screen z-10 bg-neutral-900 bg-opacity-95 flex justify-evenly items-center">
+      {prevImage ? (
+        <div
+          className="cursor-pointer bg-black p-2 rounded-lg"
+          onClick={() => changeImage(prevImage)}
+        >
+          <AiOutlineLeft size={20} />
+        </div>
+      ) : (
+        <div className="invisible p-2">
+          <AiOutlineLeft size={20} />
+        </div>
+      )}
+
+      <div className="relative w-[90%] h-full select-none">
+        <Image
+          src={currentImage.src}
+          alt={currentImage.alt}
+          fill
+          style={{ objectFit: "contain" }}
+        />
+      </div>
+
+      {nextImage ? (
+        <div
+          className="cursor-pointer bg-black p-2 rounded-lg"
+          onClick={() => changeImage(nextImage)}
+        >
+          <AiOutlineRight size={20} />
+        </div>
+      ) : (
+        <div className="invisible p-2">
+          <AiOutlineRight size={20} />
+        </div>
+      )}
+
+      <div
+        onClick={closeImage}
+        className="absolute top-[3%] right-[1.5%] cursor-pointer"
+      >
+        <AiOutlineClose size={20} />
+      </div>
+    </div>
+  );
+};
+
+export default FullImageGallery;
