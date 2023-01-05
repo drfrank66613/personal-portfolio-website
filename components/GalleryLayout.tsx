1b670688f1663078da9a6adfbc30d6dce8039cda
drfrank66613 <dm66613@gmail.com>
2023-01-05 20:23:10 +0700

    Update GalleryLayout.tsx
    -use flex instead of grid for the layout
    -add in scroll on mousemove functionality

--- components/GalleryLayout.tsx
+++ components/GalleryLayout.tsx
@@ -1,5 +1,9 @@
+import { useState, MouseEvent, useRef, useEffect } from "react";
 import { ImageGallery } from "../data/projects";
 import GalleryThumb from "./GalleryThumb";
+import Image from "next/image";
+import { gsap } from "gsap";
+import { useRouter } from "next/router";
 
 type GalleryLayoutProps = {
   gallery: ImageGallery[];
@@ -7,12 +11,66 @@ type GalleryLayoutProps = {
 };
 
 const GalleryLayout = ({ gallery, viewImage }: GalleryLayoutProps) => {
+  const router = useRouter();
+  const container = useRef<HTMLDivElement>(null);
+  const content = useRef<HTMLDivElement>(null);
+  const [panX, setPanX] = useState<number>(0);
+
+  const main = gallery.find((_, index) => index === 0);
+  const sides = gallery.filter((_, index) => index !== 0);
+
+  const onMouseMove = (e: MouseEvent) => {
+    const containerMouseX = e.clientX - container.current?.offsetLeft!;
+
+    const xDecimal = containerMouseX / container.current?.offsetWidth!;
+
+    const maxX =
+      content.current?.offsetWidth! - container.current?.offsetWidth!;
+
+    setPanX(maxX * xDecimal * -1);
+
+    gsap.to(content.current, {
+      duration: 1,
+      overwrite: true,
+      ease: "power3",
+      x: panX,
+    });
+  };
+
+  useEffect(() => {
+    setPanX(0);
+
+    gsap.to(content.current, {
+      duration: 0,
+      overwrite: true,
+      ease: "power3",
+      x: 0,
+    });
+  }, [router.query.projectId]);
+
   return (
-    <div className="grid grid-cols-2 grid-rows-3 gap-1 h-full">
-      {gallery.map((image, index) => (
-        <GalleryThumb index={index} image={image} viewImage={viewImage} />
-      ))}
-    </div>
+    <>
+      {main && (
+        <div className="flex flex-col h-full space-y-1">
+          <div className="h-[60%] border rounded-lg cursor-pointer">
+            <GalleryThumb image={main} viewImage={viewImage} />
+          </div>
+          <div
+            ref={container}
+            onMouseMove={onMouseMove}
+            className="grow overflow-hidden border-x rounded-lg"
+          >
+            <div ref={content} className="flex space-x-1 w-fit h-full">
+              {sides.map((image) => (
+                <div className="w-[290px] h-full border rounded-lg cursor-pointer">
+                  <GalleryThumb image={image} viewImage={viewImage} />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
