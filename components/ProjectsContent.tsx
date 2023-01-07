19234a63d56839698fd10a15f1f3a5418fd6e804
drfrank66613 <dm66613@gmail.com>
2023-01-07 17:39:49 +0700

    Update ProjectsContent.tsx
    modify code for logic of scrolling content on mouse move

--- components/ProjectsContent.tsx
+++ components/ProjectsContent.tsx
@@ -5,70 +5,82 @@ import ProjectThumb from "./ProjectThumb";
 import { Projects } from "../data/projects";
 
 const ProjectsContent = () => {
-  const scroller = useRef<HTMLDivElement>(null);
+  // const scroller = useRef<HTMLDivElement>(null);
+  const container = useRef<HTMLDivElement>(null);
+  const content = useRef<HTMLDivElement>(null);
+  const [panX, setPanX] = useState<number>(0);
   const [contentWidth, setContentWidth] = useState<number>(0);
   const [windowWidth, setWindowWidth] = useState<number>(0);
   const [overflow, setOverflow] = useState<number>(0);
   const [mapPosition, setMapPosition] = useState<Function>(Function);
 
-  const initMapPosition = () =>
-    gsap.utils.mapRange(0, windowWidth, overflow / 2, overflow / -2);
+  // const initMapPosition = () =>
+  //   gsap.utils.mapRange(0, windowWidth, overflow / 2, overflow / -2);
 
-  const onResize = () => {
-    setWindowWidth(window.innerWidth);
-    setOverflow(contentWidth - windowWidth);
-    setMapPosition(initMapPosition);
-  };
+  // const onResize = () => {
+  //   setWindowWidth(window.innerWidth);
+  //   setOverflow(contentWidth - windowWidth);
+  //   setMapPosition(initMapPosition);
+  // };
 
   const onMouseMove = (e: MouseEvent) => {
-    if (overflow > 0) {
-      let x = e.clientX || 0;
+    const containerMouseX = e.clientX - container.current?.offsetLeft!;
 
-      gsap.to(scroller.current, {
-        duration: 1,
-        overwrite: true,
-        ease: "power3",
-        x: mapPosition(x),
-      });
+    const xDecimal = containerMouseX / container.current?.offsetWidth!;
 
-      return;
-    }
+    const maxX =
+      content.current?.offsetWidth! - container.current?.offsetWidth!;
 
-    gsap.to(scroller.current, {
+    setPanX(maxX * xDecimal * -1);
+
+    gsap.to(content.current, {
       duration: 1,
       overwrite: true,
       ease: "power3",
-      x: 0,
+      x: panX,
     });
+    // if (overflow > 0) {
+    //   let x = e.clientX || 0;
+
+    //   gsap.to(scroller.current, {
+    //     duration: 1,
+    //     overwrite: true,
+    //     ease: "power3",
+    //     x: mapPosition(x),
+    //   });
+
+    //   return;
+    // }
+
+    // gsap.to(scroller.current, {
+    //   duration: 1,
+    //   overwrite: true,
+    //   ease: "power3",
+    //   x: 0,
+    // });
   };
 
-  useEffect(() => {
-    setContentWidth(scroller.current?.offsetWidth!);
-    setWindowWidth(window.innerWidth);
-    setOverflow(contentWidth - windowWidth);
-    setMapPosition(initMapPosition);
+  // useEffect(() => {
+  //   setPanX(0);
 
-    window.addEventListener("resize", onResize);
-
-    return () => {
-      window.removeEventListener("resize", onResize);
-    };
-  }, [windowWidth, contentWidth, overflow]);
+  //   gsap.to(content.current, {
+  //     duration: 0,
+  //     overwrite: true,
+  //     ease: "power3",
+  //     x: 0,
+  //   });
+  // }, [windowWidth, contentWidth, overflow]);
 
   return (
     <div
-      className="h-full w-full flex items-center justify-center overflow-hidden relative border-x-2 rounded-lg"
+      ref={container}
+      className="h-full w-full overflow-hidden border-x-2 rounded-lg"
       onMouseMove={onMouseMove}
     >
-      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 h-full">
-        <div
-          className="flex justify-center items-center text-black h-full"
-          ref={scroller}
-        >
-          {Projects.map(({ id, name }, index) => (
-            <ProjectThumb key={index} src={`/${id}.png`} alt={name} id={id} />
-          ))}
-        </div>
+      <div className="flex h-full space-x-2 w-fit text-black" ref={content}>
+        {Projects.map(({ id, name }, index) => (
+          <ProjectThumb key={index} src={`/${id}.png`} alt={name} id={id} />
+        ))}
       </div>
     </div>
   );
