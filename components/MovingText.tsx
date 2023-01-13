7c7feb889910bd6bd62c981d3464f3d0e7a855dd
drfrank66613 <dm66613@gmail.com>
2023-01-13 18:30:38 +0700

    Update MovingText.tsx
    apply more responsive design

--- components/MovingText.tsx
+++ components/MovingText.tsx
@@ -3,7 +3,7 @@ import Marquee from "react-fast-marquee";
 const MovingText = () => {
   return (
     <h1
-      className="rotate-180 text-right text-3xl"
+      className="rotate-180 text-right text-3xl md:text-2xl md:leading-6 lg:text-3xl xl:text-4xl xl:leading-8"
       style={{ writingMode: "vertical-lr" }}
     >
       George's Portfolio
