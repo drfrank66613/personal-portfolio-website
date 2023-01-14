a7494b1065f2b81c85d2ff857c29485175bf0905
drfrank66613 <dm66613@gmail.com>
2023-01-14 17:42:34 +0700

    Update MovingText.tsx
    apply more responsive design

--- components/MovingText.tsx
+++ components/MovingText.tsx
@@ -1,10 +1,16 @@
 import Marquee from "react-fast-marquee";
+import { useMediaQuery } from "react-responsive";
 
 const MovingText = () => {
+  const md = useMediaQuery({ minWidth: 768 });
+  const customStyle = md ? "rotate-180 text-right" : "";
+
   return (
     <h1
-      className="rotate-180 text-right text-3xl md:text-2xl md:leading-6 lg:text-3xl xl:text-4xl xl:leading-8"
-      style={{ writingMode: "vertical-lr" }}
+      className={`${customStyle} text-xl md:text-2xl md:leading-6 lg:text-3xl xl:text-4xl xl:leading-8`}
+      style={
+        md ? { writingMode: "vertical-lr" } : { writingMode: "horizontal-tb" }
+      }
     >
       George's Portfolio
     </h1>
