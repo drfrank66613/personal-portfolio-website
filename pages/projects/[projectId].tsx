838a3182c50568bf843f4e8f714c18bc42eda35c
drfrank66613 <dm66613@gmail.com>
2023-01-20 18:09:46 +0700

    Update [projectId].tsx
    apply more responsive design

--- pages/projects/[projectId].tsx
+++ pages/projects/[projectId].tsx
@@ -1,4 +1,11 @@
-import { ReactElement, useEffect, useState } from "react";
+import {
+  ReactElement,
+  useEffect,
+  useRef,
+  useState,
+  WheelEvent,
+  MouseEvent,
+} from "react";
 import { NextPageWithLayout } from "../_app";
 import { BiLeftArrow, BiRightArrow } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
@@ -11,12 +18,16 @@ import FullImageGallery from "../../components/FullImageGallery";
 import type { ImageGallery } from "../../data/projects";
 import GalleryLayout from "../../components/GalleryLayout";
 import Link from "next/link";
+import { useMediaQuery } from "react-responsive";
 
 const ProjectDetails: NextPageWithLayout = () => {
   const router = useRouter();
   const [isContentExpanded, setIsContentExpanded] = useState<boolean>(false);
   const [viewedImage, setViewedImage] = useState<ImageGallery | null>(null);
   const [isFullImageOpen, setIsFullImageOpen] = useState<boolean>(false);
+  const scrollableContent = useRef<HTMLDivElement>(null);
+  const scrollableLabel = useRef<HTMLDivElement>(null);
+  const md = useMediaQuery({ minWidth: 768 });
 
   const currentProject = Projects.find(
     (project) => project.id === router.query.projectId
@@ -48,6 +59,28 @@ const ProjectDetails: NextPageWithLayout = () => {
     setIsContentExpanded(false);
   }, [router.query.projectId]);
 
+  const preventDefault = (e: any) => e.preventDefault();
+
+  const disableContentScrollable = () => {
+    scrollableContent.current?.addEventListener("wheel", preventDefault, {
+      passive: false,
+    });
+  };
+
+  const enableContentScrollable = () => {
+    scrollableContent.current?.removeEventListener(
+      "wheel",
+      preventDefault,
+      false
+    );
+  };
+
+  const onLabelWheel = (e: WheelEvent<HTMLDivElement>) => {
+    if (scrollableLabel.current) {
+      scrollableLabel.current.scrollLeft += e.deltaY;
+    }
+  };
+
   return (
     <>
       {currentProject && (
@@ -62,23 +95,39 @@ const ProjectDetails: NextPageWithLayout = () => {
             </Link>
           </header>
 
-          <main className="h-[90%] flex space-x-10">
-            <section className="w-[60%] flex items-center">
-              <div className="content-border max-h-[75%] overflow-auto pr-3">
+          <main className="h-[90%] md:flex md:space-x-10">
+            <section className="md:w-[60%] md:h-full h-[50%] flex items-center">
+              <div
+                className="content-border max-h-[75%] overflow-y-auto overflow-x-hidden pr-3"
+                ref={scrollableContent}
+              >
                 <h1>{currentProject.name}</h1>
-                <div>
-                  {currentProject.skills.map((skill, index, skills) => {
-                    const divider = index + 1 === skills.length ? "" : " | ";
+                <div className="relative">
+                  <div
+                    className="whitespace-nowrap md:w-full md:whitespace-normal overflow-x-hidden"
+                    ref={scrollableLabel}
+                    onWheel={onLabelWheel}
+                    onMouseEnter={disableContentScrollable}
+                    onMouseLeave={enableContentScrollable}
+                  >
+                    {currentProject.skills.map((skill, index, skills) => {
+                      const divider = index + 1 === skills.length ? "" : " | ";
 
-                    return (
-                      <label
-                        key={skill}
-                        className="skills-list text-xs lg:text-sm text-neutral-400"
-                      >
-                        {skill + divider}
-                      </label>
-                    );
-                  })}
+                      return (
+                        <label
+                          key={skill}
+                          className="skills-list text-xs lg:text-sm text-neutral-400"
+                        >
+                          {skill + divider}
+                        </label>
+                      );
+                    })}
+                  </div>
+                  {md || (
+                    <div className="absolute inset-y-0 right-0 bg-black w-5 flex justify-center items-center">
+                      <div>r</div>
+                    </div>
+                  )}
                 </div>
                 <div className="mt-3">
                   {currentProject.content.initial}
@@ -89,7 +138,7 @@ const ProjectDetails: NextPageWithLayout = () => {
                 </div>
               </div>
             </section>
-            <section className="w-[40%] py-20">
+            <section className="md:w-[40%] md:h-full h-[50%] md:py-20">
               <GalleryLayout
                 gallery={currentProject.gallery}
                 viewImage={viewImage}
