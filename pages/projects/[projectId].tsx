d40944fd4bb8f9400537a55a8f906e80e980be32
drfrank66613 <dm66613@gmail.com>
2023-01-21 19:45:28 +0700

    Update [projectId].tsx
    apply more responsive design

--- pages/projects/[projectId].tsx
+++ pages/projects/[projectId].tsx
@@ -19,6 +19,7 @@ import type { ImageGallery } from "../../data/projects";
 import GalleryLayout from "../../components/GalleryLayout";
 import Link from "next/link";
 import { useMediaQuery } from "react-responsive";
+import SkillsLabel from "../../components/SkillsLabel";
 
 const ProjectDetails: NextPageWithLayout = () => {
   const router = useRouter();
@@ -26,8 +27,7 @@ const ProjectDetails: NextPageWithLayout = () => {
   const [viewedImage, setViewedImage] = useState<ImageGallery | null>(null);
   const [isFullImageOpen, setIsFullImageOpen] = useState<boolean>(false);
   const scrollableContent = useRef<HTMLDivElement>(null);
-  const scrollableLabel = useRef<HTMLDivElement>(null);
-  const md = useMediaQuery({ minWidth: 768 });
+  const sm = useMediaQuery({ minWidth: 640 });
 
   const currentProject = Projects.find(
     (project) => project.id === router.query.projectId
@@ -59,28 +59,6 @@ const ProjectDetails: NextPageWithLayout = () => {
     setIsContentExpanded(false);
   }, [router.query.projectId]);
 
-  const preventDefault = (e: any) => e.preventDefault();
-
-  const disableContentScrollable = () => {
-    scrollableContent.current?.addEventListener("wheel", preventDefault, {
-      passive: false,
-    });
-  };
-
-  const enableContentScrollable = () => {
-    scrollableContent.current?.removeEventListener(
-      "wheel",
-      preventDefault,
-      false
-    );
-  };
-
-  const onLabelWheel = (e: WheelEvent<HTMLDivElement>) => {
-    if (scrollableLabel.current) {
-      scrollableLabel.current.scrollLeft += e.deltaY;
-    }
-  };
-
   return (
     <>
       {currentProject && (
@@ -96,39 +74,16 @@ const ProjectDetails: NextPageWithLayout = () => {
           </header>
 
           <main className="h-[90%] md:flex md:space-x-10">
-            <section className="md:w-[60%] md:h-full h-[50%] flex items-center">
+            <section className="md:w-[60%] sm:h-[50%] md:h-full h-[60%] flex items-center">
               <div
                 className="content-border max-h-[75%] overflow-y-auto overflow-x-hidden pr-3"
                 ref={scrollableContent}
               >
                 <h1>{currentProject.name}</h1>
-                <div className="relative">
-                  <div
-                    className="whitespace-nowrap md:w-full md:whitespace-normal overflow-x-hidden"
-                    ref={scrollableLabel}
-                    onWheel={onLabelWheel}
-                    onMouseEnter={disableContentScrollable}
-                    onMouseLeave={enableContentScrollable}
-                  >
-                    {currentProject.skills.map((skill, index, skills) => {
-                      const divider = index + 1 === skills.length ? "" : " | ";
-
-                      return (
-                        <label
-                          key={skill}
-                          className="skills-list text-xs lg:text-sm text-neutral-400"
-                        >
-                          {skill + divider}
-                        </label>
-                      );
-                    })}
-                  </div>
-                  {md || (
-                    <div className="absolute inset-y-0 right-0 bg-black w-5 flex justify-center items-center">
-                      <div>r</div>
-                    </div>
-                  )}
-                </div>
+                <SkillsLabel
+                  project={currentProject}
+                  scrollableContent={scrollableContent}
+                />
                 <div className="mt-3">
                   {currentProject.content.initial}
                   {isContentExpanded ? currentProject.content.full : null}
@@ -138,7 +93,7 @@ const ProjectDetails: NextPageWithLayout = () => {
                 </div>
               </div>
             </section>
-            <section className="md:w-[40%] md:h-full h-[50%] md:py-20">
+            <section className="md:w-[40%] sm:h-[50%] md:h-full h-[40%] md:py-32 lg:py-20">
               <GalleryLayout
                 gallery={currentProject.gallery}
                 viewImage={viewImage}
@@ -159,7 +114,7 @@ const ProjectDetails: NextPageWithLayout = () => {
                 className="flex items-center space-x-2 cursor-pointer"
               >
                 <BiLeftArrow size={20} />
-                <h2>{prevProject.name}</h2>
+                {sm ? <h2>{prevProject.name}</h2> : <h2>Prev</h2>}
               </Link>
             ) : (
               <div></div>
@@ -170,7 +125,7 @@ const ProjectDetails: NextPageWithLayout = () => {
                 href={`/projects/${nextProject.id}`}
                 className="flex items-center space-x-2 cursor-pointer"
               >
-                <h2>{nextProject.name}</h2>
+                {sm ? <h2>{nextProject.name}</h2> : <h2>Next</h2>}
                 <BiRightArrow size={20} />
               </Link>
             ) : (
