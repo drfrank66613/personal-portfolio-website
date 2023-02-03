66146be9ae778c33ffacf8b51d1a6214877a7569
drfrank66613 <dm66613@gmail.com>
2023-02-03 20:00:43 +0700

    Update GalleryThumb.tsx
    -import ReactPlayer
    -modify logic (display GalleryThumb based on item gallery's type

--- components/GalleryThumb.tsx
+++ components/GalleryThumb.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { useState } from "react";
 import type { ImageGallery } from "../data/projects";
+import ReactPlayer from "react-player/youtube";
 
 type GalleryThumbProps = {
   image: ImageGallery;
@@ -15,25 +16,43 @@ const GalleryThumb = ({ image, viewImage }: GalleryThumbProps) => {
   };
 
   return (
-    <div
-      onMouseEnter={toggleActive}
-      onMouseLeave={toggleActive}
-      onClick={() => viewImage(image)}
-      className="relative h-full"
-    >
-      {isActive && (
-        <div className="absolute bg-neutral-900 bg-opacity-90 rounded-lg flex justify-center items-center h-full w-full z-10">
-          <h2 className="text-white px-2 py-1 rounded-lg">Click to view</h2>
+    <>
+      {image.type === "image" && (
+        <div
+          onMouseEnter={toggleActive}
+          onMouseLeave={toggleActive}
+          onClick={() => viewImage(image)}
+          className="relative h-full"
+        >
+          {isActive && (
+            <div className="absolute bg-neutral-900 bg-opacity-90 rounded-lg flex justify-center items-center h-full w-full z-10">
+              <h2 className="text-white px-2 py-1 rounded-lg">Click to view</h2>
+            </div>
+          )}
+
+          <Image
+            src={image.src}
+            alt={image.alt}
+            fill
+            style={{ objectFit: "cover", borderRadius: "0.5rem" }}
+          />
         </div>
       )}
-
-      <Image
-        src={image.src}
-        alt={image.alt}
-        fill
-        style={{ objectFit: "cover", borderRadius: "0.5rem" }}
-      />
-    </div>
+      {image.type === "video" && (
+        <div className="relative h-full">
+          <ReactPlayer
+            url={image.src}
+            height="100%"
+            width="100%"
+            style={{
+              overflow: "hidden",
+              borderRadius: "0.5rem",
+            }}
+            controls
+          />
+        </div>
+      )}
+    </>
   );
 };
 
