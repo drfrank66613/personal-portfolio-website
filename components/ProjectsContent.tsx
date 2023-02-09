723381a1e5218a7c8b15cdf48dbbda04a8af7876
drfrank66613 <dm66613@gmail.com>
2023-02-09 18:14:02 +0700

    Update ProjectsContent.tsx
    fix glitch behavior of drag animation (still has minor bug)

--- components/ProjectsContent.tsx
+++ components/ProjectsContent.tsx
@@ -1,4 +1,4 @@
-import { useRef, useState, PointerEvent } from "react";
+import { useRef, useState, PointerEvent, useEffect } from "react";
 import ProjectThumb from "./ProjectThumb";
 import { Projects } from "../data/projects";
 import { motion, useDragControls } from "framer-motion";
@@ -7,6 +7,9 @@ const ProjectsContent = () => {
   const container = useRef<HTMLDivElement>(null);
   const content = useRef<HTMLDivElement>(null);
   const [panX, setPanX] = useState<number>(0);
+  const [isMomentumOn, setIsMomentumOn] = useState<boolean>(true);
+  const [leftConstraint, setLeftConstraint] = useState<number>(2); // still need to figure out how to get the dynamic value
+  const [rightConstraint, setRightConstraint] = useState<number>(0);
 
   const onPointerMove = (e: PointerEvent) => {
     if (e.pointerType === "mouse") {
@@ -21,6 +24,32 @@ const ProjectsContent = () => {
     }
   };
 
+  const toggleMomentum = (e: PointerEvent) => {
+    if (e.pointerType === "touch") {
+      const x = Number(
+        content.current?.style.transform
+          .split(" ")[0]
+          .replace(/[^0-9\.]+/g, "")!
+      );
+
+      if (x > rightConstraint) {
+        setRightConstraint(x);
+      }
+
+      if (x === leftConstraint) {
+        setIsMomentumOn(false);
+        return;
+      }
+
+      if (x === rightConstraint) {
+        setIsMomentumOn(false);
+        return;
+      }
+
+      setIsMomentumOn(true);
+    }
+  };
+
   return (
     <div
       ref={container}
@@ -31,12 +60,8 @@ const ProjectsContent = () => {
         drag="x"
         dragConstraints={container}
         dragElastic={0}
-        dragMomentum={false}
-        onDrag={(e, info) => {
-          console.log(e);
-          // let target = e.target as HTMLImageElement
-          // if(target.x )
-        }}
+        dragMomentum={isMomentumOn}
+        onPointerMove={toggleMomentum}
         dragTransition={{ timeConstant: 200, power: 0.2 }}
         animate={{ x: panX }}
         transition={{ ease: "easeOut", duration: 0.5 }}
